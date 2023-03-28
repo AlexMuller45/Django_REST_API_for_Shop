3ca834a2690acc507f6335cc834022f5c977db7c
Aleksey Melnikov <mas-chel@mail.ru>
2023-03-28 15:23:13 +0500

    Stage 2. Profile and Cart. Добавлена логика корзины

--- diploma_backend/app_cart/models.py
+++ diploma_backend/app_cart/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from app_megano.models import Products, Category
+
+
+class CartItems(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    item_id = models.CharField(max_length=64, unique=True, null=False, blank=False, verbose_name='ID товара')
+    category = models.CharField(max_length=16, unique=True, null=False, blank=False, verbose_name='ID категории')
+    price = models.DecimalField(max_digits=9, decimal_places=2, default=0, verbose_name='Стоимость единицы товара')
+    count = models.PositiveSmallIntegerField(default=0, verbose_name='Количество товара')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления товара')
+    freeDelivery = models.BooleanField(default=False, verbose_name='Бесплатная доставка есть/нет')
+
+    # def __str__(self):
+    #     obj = Products.objects.get(pk=int(self.item_id)).values('title')
+    #     return obj.title
+
--- diploma_backend/app_cart/views.py
+++ diploma_backend/app_cart/views.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import viewsets, generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from app_megano.models import Products, Category
+from app_cart.models import CartItems
+from app_cart.CartServices import CartService
+from app_cart.serializers import *
+
+
+class CartItemView(ListAPIView):
+    queryset = CartItem.objects.filter(user=self.request.user)
+    serializer_class = CartItemSerializer
+    permission_classes = (IsAuthenticated, )
+
+
+class CartItemAddView(CreateAPIView):
+    queryset = CartItem.objects.filter(user=self.request.user)
+    serializer_class = CartItemAddSerializer
+    permission_classes = (IsAuthenticated, )
+
+
+class CartItemDelView(DestroyAPIView):
+    permission_classes = (IsAuthenticated, )
+    queryset = CartItem.objects.filter(user=self.request.user)
+
+    def delete(self, request, pk, format=None):
+        user = request.user
+        cart_item = CartItem.objects.filter(user=user)
+        target_product = get_object_or_404(cart_item, pk=pk)
+        product = get_object_or_404(Product, id=target_product.product.id)
+        product.quantity = product.quantity + target_product.quantity
+        product.save()
+        target_product.delete()
+        return Response(status=status.HTTP_200_OK, data={"detail": "deleted"})
